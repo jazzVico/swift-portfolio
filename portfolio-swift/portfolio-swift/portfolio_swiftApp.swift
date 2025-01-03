07ee02aa51fec21ec813c46dafad2206c7ce8e1a
jazminvico <jazminalvarezvico@gmail.com>
2025-01-03 13:01:40 -0300

    init project

---
+++ portfolio-swift/portfolio-swift/portfolio_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  portfolio_swiftApp.swift
+//  portfolio-swift
+//
+//  Created by Jazmin Vico on 03/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct portfolio_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
